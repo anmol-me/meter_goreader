de65434ac3c3bea0860550f1389b9dde4d4f23f5
Anmol <anmol.sin@gmail.com>
2024-01-03 11:34:19 +0530

    initial

---
+++ android/app/src/main/kotlin/com/anisingh/meter_reader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anisingh.meter_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meter Reader',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Colors.white),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          background: Colors.white,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+    );
+  }
+}
